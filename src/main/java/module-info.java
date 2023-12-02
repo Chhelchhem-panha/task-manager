385c96166d625876b71137d0bbf1c78d75ef6dd0
Panha <chhelpanha2@gmail.com>
2023-12-02 19:02:51 +0700

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.panhacode.taskmanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.panhacode.taskmanager to javafx.fxml;
+    exports com.panhacode.taskmanager;
+}
